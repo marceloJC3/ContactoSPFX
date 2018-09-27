cb48969a795fce5c196422c369cbf3b0ed260cd4
Marcelo Mercado Godoy <mmercado@LATINSHARE.COM>
2018-09-27 11:17:53 -0300

    Carga Inicial

---
+++ src/webparts/contacto/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IContactoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ContactoWebPartStrings' {
+  const strings: IContactoWebPartStrings;
+  export = strings;
+}
